73447d2dbfe1e623e92e0134d768156239702e02
password123456 <sendmefuture@gmail.com>
2023-08-28 08:58:19 +0900

    Update and rename get_all_user_list_of_channel.py to main.py

--- retrieve_the_list_of_users_who_have_joined_on_channel/main.py
+++ retrieve_the_list_of_users_who_have_joined_on_channel/main.py
@@ -1,5 +1,5 @@
 __author__ = 'https://github.com/password123456/'
-__version__ = '1.0.1-230201'
+__version__ = '1.0.2-230828'
 
 import sys
 from slack_sdk import WebClient
@@ -20,29 +20,29 @@ class Bcolors:
     UNDERLINE = '\033[4m'
 
 
-def get_user_list_of_channel(token, channel_id):
+def get_list_of_users_on_channel(token, channel_id):
     client = WebClient(token=token)
-
     try:
-        result = client.conversations_members(channel=channel_id)
+        # https://api.slack.com/methods/conversations.members
+        result = client.conversations_members(channel=channel_id, limit=400)
+        # print(result['members'])
         i = 0
         for user in result['members']:
             try:
+                # https://api.slack.com/methods/users.info
                 info = client.users_info(user=user).data
                 if not info['user']['is_bot']:
                     i = i + 1
-
                     member_id = info['user']['id']
-                    team_id = info['user']['team_id']
+                    # team_id = info['user']['team_id']
                     display_name = info['user']['name']
                     real_name = info['user']['real_name']
                     phone = info['user']['profile']['phone']
                     email = info['user']['profile']['email']
+                    title = info['user']['profile']['title']
 
                     if not member_id:
                         member_id = 'null'
-                    elif not team_id:
-                        team_id = 'null'
                     elif not display_name:
                         display_name = 'null'
                     elif not real_name:
@@ -52,17 +52,19 @@ def get_user_list_of_channel(token, channel_id):
                     elif not email:
                         email = 'null'
 
-                    print(f'{i},{real_name},{display_name},{team_id},{member_id},{email},{phone}')
+                    print(f'{i},{real_name},{display_name},{title},{member_id},{email},{phone}')
             except SlackApiError as e:
                 print(f'{Bcolors.Yellow}- Api Error:: {e} {Bcolors.Endc}')
     except SlackApiError as e:
         print(f'{Bcolors.Yellow}- Api Error:: {e} {Bcolors.Endc}')
 
-def main():
-    bot_token = 'YOUR_BOT_TOKEN'
-    channel = 'channel_id_you_want_to_know'
 
-    get_user_list_of_channel(bot_token, channel)
+def main():
+    bot_token = 'slack_apps_oauth_token(bot_token)'
+    channel = 'Channel number for which you want to know the list of users'
+
+    get_list_of_users_on_channel(bot_token, channel)
+
 
 if __name__ == '__main__':
     try:
@@ -72,4 +74,3 @@ if __name__ == '__main__':
     except Exception as e:
         print(f'{Bcolors.Yellow}- ::Exception:: Func:[{__name__.__name__}] '
               f'Line:[{sys.exc_info()[-1].tb_lineno}] [{type(e).__name__}] {e}{Bcolors.Endc}')
-
