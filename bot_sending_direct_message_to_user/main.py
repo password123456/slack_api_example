a4c9385dc6a282259f396f8ad55b1779998061a6
password123456 <sendmefuture@gmail.com>
2023-08-29 13:46:49 +0900

    Update main.py

--- bot_sending_direct_message_to_user/main.py
+++ bot_sending_direct_message_to_user/main.py
@@ -1,5 +1,5 @@
 __author__ = 'https://github.com/password123456/'
-__version__ = '1.0.0-230828'
+__version__ = '1.0.1-230829'
 
 import sys
 from slack_sdk import WebClient
@@ -25,21 +25,20 @@ def conversations_open(bot_token, user_id):
     client = WebClient(token=bot_token)
     try:
         result = client.conversations_open(users=user_id)
-        channel_id = result['channel']['id']
-        if channel_id:
-            return channel_id
+        if result['ok']:
+            return result['channel']['id']
         else:
             return None
     except SlackApiError as e:
         print(f'{Bcolors.Yellow}- Api Error:: {e} {Bcolors.Endc}')
         return None
-
+        
 
 def chat_post_message(bot_token, channel_id, message):
     # https://api.slack.com/methods/chat.postMessage
     client = WebClient(token=bot_token)
     try:
-        result = client.chat_postMessage(token=bot_token, channel=channel_id, text=message, as_user=True)
+        result = client.chat_postMessage(channel=channel_id, text=message, as_user=True)
         print(result)
     except SlackApiError as e:
         print(f'{Bcolors.Yellow}- Api Error:: {e} {Bcolors.Endc}')
@@ -49,10 +48,10 @@ def main():
     bot_token = 'slack_apps_oauth_token(bot_token)'
     user_id = 'User ID to which the bot can send direct messages.'
   
-    conversation_id = conversations_open(bot_token, user_id)
-    if conversation_id:
+    chat_id = conversations_open(bot_token, user_id)
+    if chat_id:
         message = f'<@{user_id}> hello my freiend.'
-        chat_post_message(bot_token, conversation_id, message)
+        chat_post_message(bot_token, chat_id, message)
 
 
 if __name__ == '__main__':
